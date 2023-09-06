379bd7b3485b7e28eb0c86c4093d5902645b84bd
yzhu71 <zhuyh7@outlook.com>
2023-09-05 22:59:33 -0700

    Add DSS

---
+++ DSS/DSS.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+library(socviz)
+
+setwd("~/Desktop/UC Merced/PhD/2.First Year/POLI210/DSS")
+
+star <- read.csv("STAR.csv")
+
+View(star)
+
+head(star)
+
+head(star, n=3)
+
+tail(star)
+
+dim(star)
+
+star$reading
+
+mean(star$reading)
+
+mean(star$graduated)
+
+install
+
+setwd("~/Desktop/Git")
